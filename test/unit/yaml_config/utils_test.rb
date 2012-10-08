8577bc44d2e44534263ae2a5baef5c6fbc34ab26
Dave Benvenuti <dave@backupify.com>
2012-10-08 11:51:11 -0400

    yaml_config will now evaluate erb.  also added some actual tests

---
+++ test/unit/yaml_config/utils_test.rb
@@ -0,0 +1,27 @@
+module YamlConfig
+
+  class UtilsTest < Test::Unit::TestCase
+
+    should 'read yml from file' do
+      yml_data = Utils.from_file(fixtures_root + "/foo.yml")
+
+      assert_equal ['first_value', 'second_value'], yml_data.array_value
+      assert_equal 'a string', yml_data.string_value
+    end
+
+    should 'read yml from file and evaluate erb' do
+      yml_data = Utils.from_file(fixtures_root + "/foo-with-erb.yml")
+
+      assert_equal 2, yml_data.a_value_from_erb
+      assert_nil yml_data.should_not_exist
+    end
+
+    should 'read yml from file and evaluate inline ruby' do
+      yml_data = Utils.from_file(fixtures_root + "/foo-with-inline-ruby.yml")
+
+      assert_equal '3', yml_data.a_value_from_inline_ruby
+    end
+
+  end
+
+end
--- test/yaml_config_test.rb
+++
@@ -1,5 +0,0 @@
-require 'helper'
-
-class TestYamlConfig < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" 
-end
